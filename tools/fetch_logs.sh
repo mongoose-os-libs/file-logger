e27a7f03201d1229368a44ba92fc4671c96dd81d
Deomid "rojer" Ryabkov <rojer@rojer.me>
2020-01-26 02:10:38 +0000

    Remove some noise

--- tools/fetch_logs.sh
+++ tools/fetch_logs.sh
@@ -3,9 +3,9 @@
 # Set MOS_PORT environment variable to change port from default.
 
 # Fetching logs may produce logs, especially at higher logging levels, so turn file logging off while fetching.
-mos call Config.Set '{"config": {"file_logger": {"enable": false } }, "reboot": false, "save": false}'
+mos call Config.Set '{"config": {"file_logger": {"enable": false } }, "reboot": false, "save": false}' > /dev/null
 # FLush everything, especially important for LFS.
-mos call FileLog.Flush
+mos call FileLog.Flush > /dev/null
 while true; do
   f=$(mos call FileLog.Status | jq -r .oldest)
   if [ -z "$f" ]; then
@@ -16,4 +16,4 @@ while true; do
   mos rm "$f"
 done
 # Re-enable logs.
-mos call Config.Set '{"config": {"file_logger": {"enable": true } }, "reboot": false, "save": false}'
+mos call Config.Set '{"config": {"file_logger": {"enable": true } }, "reboot": false, "save": false}' > /dev/null
